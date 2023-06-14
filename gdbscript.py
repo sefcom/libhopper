60417c80a3c20f85240980b04310501ec8018975
Thomason Zhao <thomasonzhao@outlook.com>
2023-06-14 14:07:24 -0500

    fix miner logic errors

--- gdbscript.py
+++ gdbscript.py
@@ -63,29 +63,28 @@ class UserCall():
     def doCorruption(self):
         # Search valid arg
         for arg in self.args:
-            if arg.name not in self.targetStructs or arg.val == "0x0":
+            if arg.name not in self.targetStructs:
+                continue
+            if arg.val == "0x0":
                 continue
 
             # Start corrupt data
-            for i in range(arg.size):
+            for i in range(0, arg.size, arg.size):
                 # Attempt to corrupt data
                 gdb.execute("checkpoint")
                 gdb.execute("restart 1")
 
-                arg.setVal("A", i)
+                gdb.execute("info args")
+                arg.setVal("A" * arg.size, i)
+                gdb.execute("info args")
 
                 finishCurrentFunc()
 
                 # Examine if crash happens
                 msg = gdb.execute("info checkpoints", to_string=True)
-                print(msg)
                 assert(msg != None)
                 if "No checkpoints" in msg.strip():
                     crashHandler(self.name, arg.name, i)
-
-                    # Continue to next breakpoint
-                    finishCurrentFunc()
-                    gdb.execute("continue")
                     continue
         
                 # Restore normal execution routine
@@ -96,10 +95,10 @@ def stopHandler(event):
     if hasattr (event, 'stop_signal'):
         gdb.execute("continue")
 
-def gdbSetup():
-    gdb.execute("start")
+def gdbSetup(brkp, argv):
+    gdb.execute(f"start {argv}")
     # TODO: do better on breaking user calls
-    gdb.rbreak("^png_")
+    gdb.rbreak(f"{brkp}")
     gdb.events.stop.connect(stopHandler)
     gdb.execute("continue")
 
@@ -115,9 +114,17 @@ def crashHandler(func, arg, off):
 
 # Main logic 
 def main():
-    targetStructs = ["png_ptr", "info_ptr"]
+    # Parameters for libpng
+    # targetStructs = ["png_ptr", "info_ptr"]
+    # brkp = "^png_"
+    # argv = ""
 
-    gdbSetup()
+    # Parameters for libjpeg
+    targetStructs = ["cinfo"]
+    brkp = "^jpeg_"
+    argv = "-dct int -ppm -outfile testout.ppm  ./testorig.jpg"
+
+    gdbSetup(brkp, argv)
 
     while(True):
         funcName = gdb.selected_frame().function()
