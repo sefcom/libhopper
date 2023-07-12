e19b3b8223bf2b6f82c2add447b3b0a96938db06
Thomason Zhao <thomasonzhao@outlook.com>
2023-07-11 17:35:44 -0700

    start to revise libhopper

---
+++ debug.py
@@ -0,0 +1,6 @@
+from libhopper import analysis
+
+
+# Debug only
+if __name__ == "__main__":
+    analysis("./libapi_write.dump", 0x7fffffffda80, 0x68, 0x55555555525c)
--- libhopper.py
+++ libhopper.py
@@ -1,10 +1,11 @@
+# import gdb
 import angr
 import claripy
 
 
-if __name__ == "__main__":
+def analysis(core_dump, struct_addr, struct_size, upper_frame):
     proj_opts = {"main_opts": {"backend": "elfcore"}}
-    proj = angr.Project("./libapi_write", load_options=proj_opts)
+    proj = angr.Project(core_dump, load_options=proj_opts)
 
     state_opts = set()
     # added_options.add(angr.options.AUTO_REFS)
@@ -24,33 +25,110 @@ if __name__ == "__main__":
             pass
 
     # Overwrite internal state struct with symbolic value
-    # TODO: replace struct addr & size
-    concrete_ctx = state.memory.load(state.regs.rdi.v, 0x68)
-    # TODO: replace struct size
-    symbolic_ctx = claripy.Concat(*[state.solver.BVS(f"sym_struct_{b}", 8) for b in range(0x68)])
+    concrete_ctx = state.memory.load(struct_addr, struct_size)
+    symbolic_ctx = claripy.Concat(*[state.solver.BVS(f"sym_struct_{b}", 8) for b in range(struct_size)])
     state.solver.add(concrete_ctx == symbolic_ctx)
-    # TODO: replace struct addr
-    state.memory.store(state.regs.rdi.v, symbolic_ctx)
+    state.memory.store(struct_addr, symbolic_ctx)
 
     simgr = proj.factory.simgr(state)
-    # TODO: replace func finish addr
-    simgr.run(until=lambda sm: sm.active[0].addr == 0x55555555525c)
+    try:
+        simgr.run(until=lambda sm: sm.active[0].addr == upper_frame)
+    except:
+        print("Simulation manager errored!")
+        import IPython; IPython.embed()
 
     # Examine history
     new_state:angr.sim_state.SimState
     new_state = simgr.active[0]
-    read_events = new_state.history.filter_actions(read_from="mem")
-    write_events = new_state.history.filter_actions(write_to="mem")
+    # read_events = new_state.history.filter_actions(read_from="mem")
+    # write_events = new_state.history.filter_actions(write_to="mem")
 
-    tainted_jumps = [jump_target for jump_target in new_state.history.jump_targets if jump_target.symbolic]
-    tainted_reads = [event for event in read_events if event.is_symbolic]
-    tainted_writes = [event for event in write_events if event.is_symbolic]
+    # tainted_jumps = [jump_target for jump_target in new_state.history.jump_targets if jump_target.symbolic]
+    # tainted_reads = [event for event in read_events if event.is_symbolic]
+    # tainted_writes = [event for event in write_events if event.is_symbolic]
 
-    for i in tainted_jumps:
-        print(f"PC: {hex(i.ins_addr)} - JUMP: {i.addr}")
-    for i in tainted_reads:
-        print(f"PC: {hex(i.ins_addr)} - READ: {i.addr}")
-    for i in tainted_writes:
-        print(f"PC: {hex(i.ins_addr)} - WRITE: {i.addr}")
+    # for i in tainted_jumps:
+    #     print(f"PC: {hex(i.ins_addr)} - JUMP: {i.addr}")
+    # for i in tainted_reads:
+    #     print(f"PC: {hex(i.ins_addr)} - READ: {i.addr}")
+    # for i in tainted_writes:
+    #     print(f"PC: {hex(i.ins_addr)} - WRITE: {i.addr}")
+
+    histories = list(new_state.history.lineage)[1:]
+    solver = claripy.Solver()
     
-    input()
+    for h in histories:
+        # TODO
+        sym_reads = [e.addr for e in h.recent_events if isinstance(e, angr.state_plugins.sim_action.sim_action.SimActionData) and e.is_symbolic]
+
+def parse_args(target_structs):
+    struct_addr = None
+    struct_size = None
+
+    args = str(gdb.execute("info args", to_string=True))
+    if "No arguments" in args:
+        return struct_addr, struct_size
+
+    args = args.strip().split("\n")
+    for arg in args:
+        temp = arg.split(" ")
+        arg_name = temp[0]
+        if arg_name in target_structs:
+            struct = gdb.parse_and_eval(arg_name).dereference()
+            struct_addr = int(struct.address)
+            struct_size = struct.type.sizeof
+    
+    return struct_addr, struct_size
+
+
+if __name__ == "__main__":
+    # Config gdb before start
+    gdb_setup = (
+        "set confirm off",
+        "set verbose off",
+        "set pagination off",
+        "set print elements 0",
+        "set history save on",
+        "set output-radix 0x10",
+        "set print pretty on",
+        "set disassembly-flavor intel",
+    )
+    for cmd in gdb_setup:
+        try:
+            gdb.execute(cmd)
+        except gdb.error:
+            pass
+
+    target_structs = []
+    brkp_regex = []
+    argv = ""
+    # Parameters for libpng
+    # target_structs = ["png_ptr", "info_ptr"]
+    # brkp_regex = ["^png_"]
+    # argv = ""
+    # Parameters for zlib
+    # target_structs = ["strm", "file"]
+    # brkp_regex = ["^deflate", "^inflate", "^gz", "^compress", "^uncompress"]
+    # argv = ""
+
+    gdb.execute(f"start {argv}")
+    for regex in brkp_regex:
+        gdb.rbreak(regex)
+    gdb.execute("c")
+    
+    while True:
+        frame = gdb.selected_frame()
+
+        func_name = frame.function().name
+        core_dump = f"{func_name}.dump"
+
+        struct_addr, struct_size = parse_args(target_structs)
+
+        upper_frame = frame.older().pc()
+        
+        if struct_addr != None:
+            gdb.execute(f"gcore {core_dump}")
+            analysis(core_dump, struct_addr, struct_size, upper_frame)
+
+        gdb.execute(f"clear {func_name}")
+        gdb.execute("c")
