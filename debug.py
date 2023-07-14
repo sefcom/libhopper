c45d8f6356a6ccf7dfa402533d305df9ee60abb5
Thomason Zhao <thomasonzhao@outlook.com>
2023-07-13 18:32:11 -0700

    extend the script to dump out ranges of mem access

--- debug.py
+++ debug.py
@@ -3,4 +3,8 @@ from libhopper import analysis
 
 # Debug only
 if __name__ == "__main__":
-    analysis("./libapi_write.dump", 0x7fffffffda80, 0x68, 0x55555555525c)
+    # analysis("./libapi_name.dump", 0x7fffffffdb50, 0x78, 0x555555555228)
+    # analysis("./libapi_read.dump", 0x7fffffffdb50, 0x78, 0x555555555266)
+    # analysis("./libapi_write.dump", 0x7fffffffdb50, 0x78, 0x55555555527c)
+    analysis("./libapi_lotto.dump", 0x7fffffffdb50, 0x78, 0x5555555552a6)
+    # analysis("./libapi_exec.dump", 0x7fffffffdb50, 0x78, 0x5555555552b5)
--- libhopper.py
+++ libhopper.py
@@ -12,25 +12,26 @@ def analysis(core_dump, struct_addr, struct_size, upper_frame):
     state_opts.add(angr.options.TRACK_MEMORY_ACTIONS)
     state_opts.add(angr.options.TRACK_JMP_ACTIONS)
     
-    state:angr.sim_state.SimState
-    state = proj.factory.blank_state(add_options=state_opts)
+    begin_state:angr.sim_state.SimState
+    begin_state = proj.factory.blank_state(add_options=state_opts)
 
     # Config symbolic state
     core_obj = proj.loader.elfcore_object
     assert(core_obj != None)
     for regval in core_obj.initial_register_values():
         try:
-            state.registers.store(regval[0], regval[1])
+            begin_state.registers.store(regval[0], regval[1])
         except:
             pass
 
     # Overwrite internal state struct with symbolic value
-    concrete_ctx = state.memory.load(struct_addr, struct_size)
-    symbolic_ctx = claripy.Concat(*[state.solver.BVS(f"sym_struct_{b}", 8) for b in range(struct_size)])
-    state.solver.add(concrete_ctx == symbolic_ctx)
-    state.memory.store(struct_addr, symbolic_ctx)
+    concrete_ctx = begin_state.memory.load(struct_addr, struct_size)
+    # symbolic_ctx = claripy.Concat(*[begin_state.solver.BVS(f"sym_struct_{b}", 8) for b in range(struct_size)])
+    symbolic_ctx = claripy.BVS("sym_struct", struct_size * 8)
+    begin_state.solver.add(concrete_ctx == symbolic_ctx)
+    begin_state.memory.store(struct_addr, symbolic_ctx)
 
-    simgr = proj.factory.simgr(state)
+    simgr = proj.factory.simgr(begin_state)
     try:
         simgr.run(until=lambda sm: sm.active[0].addr == upper_frame)
     except:
@@ -38,28 +39,21 @@ def analysis(core_dump, struct_addr, struct_size, upper_frame):
         import IPython; IPython.embed()
 
     # Examine history
-    new_state:angr.sim_state.SimState
-    new_state = simgr.active[0]
-    # read_events = new_state.history.filter_actions(read_from="mem")
-    # write_events = new_state.history.filter_actions(write_to="mem")
+    end_state:angr.sim_state.SimState
+    end_state = simgr.active[0]
+    read_events = end_state.history.filter_actions(read_from="mem")
+    write_events = end_state.history.filter_actions(write_to="mem")
 
-    # tainted_jumps = [jump_target for jump_target in new_state.history.jump_targets if jump_target.symbolic]
-    # tainted_reads = [event for event in read_events if event.is_symbolic]
-    # tainted_writes = [event for event in write_events if event.is_symbolic]
+    tainted_reads = [event for event in read_events[::-1] if event.is_symbolic]
+    tainted_writes = [event for event in write_events[::-1] if event.is_symbolic]
+    tainted_jumps = [jump_target for jump_target in end_state.history.jump_targets if jump_target.symbolic]
+    constraints = [constraint.ast for constraint in end_state.history.constraints_since(begin_state)]
 
-    # for i in tainted_jumps:
-    #     print(f"PC: {hex(i.ins_addr)} - JUMP: {i.addr}")
-    # for i in tainted_reads:
-    #     print(f"PC: {hex(i.ins_addr)} - READ: {i.addr}")
-    # for i in tainted_writes:
-    #     print(f"PC: {hex(i.ins_addr)} - WRITE: {i.addr}")
-
-    histories = list(new_state.history.lineage)[1:]
     solver = claripy.Solver()
-    
-    for h in histories:
-        # TODO
-        sym_reads = [e.addr for e in h.recent_events if isinstance(e, angr.state_plugins.sim_action.sim_action.SimActionData) and e.is_symbolic]
+    solver.add(constraints)
+    read_ranges = [(solver.min(e.addr.ast), solver.max(e.addr.ast)) for e in tainted_reads]
+    write_ranges = [(solver.min(e.addr.ast), solver.max(e.addr.ast)) for e in tainted_writes]
+    jump_ranges = [(solver.min(e), solver.max(e)) for e in tainted_jumps]
 
 def parse_args(target_structs):
     struct_addr = None
@@ -99,8 +93,8 @@ if __name__ == "__main__":
         except gdb.error:
             pass
 
-    target_structs = []
-    brkp_regex = []
+    target_structs = ["state"]
+    brkp_regex = ["^libapi_"]
     argv = ""
     # Parameters for libpng
     # target_structs = ["png_ptr", "info_ptr"]
@@ -128,7 +122,7 @@ if __name__ == "__main__":
         
         if struct_addr != None:
             gdb.execute(f"gcore {core_dump}")
-            analysis(core_dump, struct_addr, struct_size, upper_frame)
+            # analysis(core_dump, struct_addr, struct_size, upper_frame)
 
         gdb.execute(f"clear {func_name}")
         gdb.execute("c")
